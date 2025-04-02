f0b4f3db2ee2abed56863c1fc760357db9333fb6
José Ismael <100933555+ismaelGTI@users.noreply.github.com>
2025-04-02 15:05:53 +0200

    Update build_deploy.sh

--- Casos/009-Github-Actions-Starting/ismaelGTI/Micros/Micro1/build_deploy.sh
+++ Casos/009-Github-Actions-Starting/ismaelGTI/Micros/Micro1/build_deploy.sh
@@ -17,3 +17,4 @@ echo "Build and deploy completed."
 echo "# Cambio de prueba" >> Casos/009-Github-Actions-Starting/ismaelGTI/Micros/Micro1/build_deploy.sh
 #TEST DEL FUNCIONAMIENTO DEL WORKFLOW.
 echo "Build and deploy completed."
+
